953f8f6dee47ec18322240676bd694b6e4cf1104
Krzysztof Skowronek <krzysztof.skowronek@relativity.com>
2021-09-24 14:04:26 +0200

    ci/cd

--- WebApplication2/Startup.cs
+++ WebApplication2/Startup.cs
@@ -32,7 +32,7 @@ namespace WebApplication2
 			{
 				endpoints.MapGet("/", async context =>
 				{
-					await context.Response.WriteAsync("Hello World!");
+					await context.Response.WriteAsync("Hello from CI/CD!");
 				});
 			});
 		}
