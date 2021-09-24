d9eea351228d51684a2ca8afb54303346cdfd1f7
Krzysztof Skowronek <krzysztof.skowronek@relativity.com>
2021-09-24 15:53:26 +0200

    ewa

--- WebApplication2/Startup.cs
+++ WebApplication2/Startup.cs
@@ -38,7 +38,7 @@ namespace WebApplication2
 
 					// pobranie sekcji "test" + fallback na wypadek jej braku
 					var testVariableValue = configuration.GetSection("test")?.Value ?? "test not configured!";
-					await context.Response.WriteAsync("<p>(App Service) Test variable: " + testVariableValue + "</p><p>Passing tests!</p>");
+					await context.Response.WriteAsync("<p>(Ewa to dupeczka) Test variable: " + testVariableValue + "</p><p>Passing tests!</p>");
 				});
 			});
 		}
